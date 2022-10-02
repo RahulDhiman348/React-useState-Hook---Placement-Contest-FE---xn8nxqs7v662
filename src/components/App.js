83ad5010047f3177074c17384dfe26ebec23b008
RahulDhiman348 <102219032+RahulDhiman348@users.noreply.github.com>
2022-10-02 14:59:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,25 @@
 
-import '../styles/App.css';
 import React, { useState } from 'react';
 
 export default function App() {
  //code here 
-
+const[string,setString]= useState("");
+const[input, setInput]= useState("");
+const text= "";
+function changeInput(event){
+setInput(event.target.value);
+}
+function buttonClick(){
+  setString(string+input);
+  setInput("");
+}
  
   return (
     <div>
-  <input id='input' value={inputValue} onChange={changeInput} />
+  <input id='input' value={input} onChange={changeInput} />
   
       <p id='intro'>Concated String</p>
-      <p id='text'>{text} </p>
+      <p id='text'>{string} </p>
       <button id='button' onClick={buttonClick}>
         Click me
       </button>
